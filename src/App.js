a1958dd47322a794e8122e337656fc4a64617201
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-15 22:33:26 +0900

    added background-gradient and animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import {Navbar} from './components/navbar'
 
 function App() {
   return (
     <div className="App">
-
+     <div className="background-gradient"></div>
+     <div className='content-container'>
+      <Navbar></Navbar>
+     </div>
+     <div className="for-ground"></div>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav> 
+            <div className='navbar'>
+                <h3>About</h3>
+                <h3>Work</h3>
+                <h3>Contant</h3>
+            </div>
+        </nav>
+    )
+}
+
+export {Navbar}

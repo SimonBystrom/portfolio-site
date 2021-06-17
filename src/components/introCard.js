3621a5f4b7528bec5433f48e7064e0b8ae8f0732
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-17 13:28:33 +0900

    Fixed background taking up all the available space

---
+++ src/components/introCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IntroCard = () => {
+    return (
+        <>
+            <h1>My name is Simon</h1>
+            <h2>I'm a fullstack developer</h2>
+        </>
+    )
+}
+
+export default IntroCard

26888dd403a47227cb376407527e130afee6372f
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-17 16:25:04 +0900

    cleaned up the font-style

--- src/components/introCard.js
+++ src/components/introCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const IntroCard = () => {
     return (
         <>
-            <h1>My name is Simon</h1>
+            <h1>Hi, I'm Simon</h1>
             <h2>I'm a fullstack developer</h2>
         </>
     )

a558a063c43c38e231711b673b239fc191e2cb9f
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-18 16:09:22 +0900

    Fixed intentions in about card

--- src/components/aboutCard.js
+++ src/components/aboutCard.js
@@ -1,40 +1,40 @@
 import React from 'react'
 
 const AboutCard = () => {
-    return(
-        <div className="about-card-container">
-            <div className="row justify-content-around align-items-end p-5">
-                <div className="col-sm-12 col-md-12 col-lg-6 about-card">
-                        <h2>About Me</h2>
-                        <h5><span>Hi, I’m Simon! </span></h5>
-                        <p>Originally from Sweden 🇸🇪, but been living in Tokyo 🇯🇵 since 2015.</p>
-                        <h5><span>Background</span></h5>
-                        <p>Animation, design and movie history but my true love is code! 
-                            I really love working on Frontend solutions and solving the puzzles of the backend.</p>
-                        <h5><span>When I’m not coding</span></h5>
-                        <p> I also love Judo, Video games and fine Cocktails!</p>
-                </div>
-                <div className="col-sm-8 col-md-12 col-lg-6 about-skills">
-                    <h2>Languages</h2>
-                    <div className="mb-5 skill-icon-container">
-                        <i className="devicon-javascript-plain"></i>
-                        <i className="devicon-html5-plain"></i>
-                        <i className="devicon-css3-plain"></i>
-                        <i className="devicon-ruby-plain"></i>
-                        <i className="devicon-postgresql-plain"></i>
-                    </div>
-                    <h2>Frameworks and Libraries</h2>
-                    <div className="skill-icon-container">
-                        <i className="devicon-react-original"></i>
-                        <i className="devicon-rails-plain"></i>
-                        <i className="devicon-bootstrap-plain"></i>
-                        <i className="devicon-git-plain"></i>
-                        <i className="devicon-github-original"></i>
-                    </div>
-                </div>
-            </div>
+  return(
+    <div className="about-card-container">
+      <div className="row justify-content-around align-items-end p-5">
+        <div className="col-sm-12 col-md-12 col-lg-6 about-card">
+                <h2>About Me</h2>
+                <h5><span>Hi, I’m Simon! </span></h5>
+                <p>Originally from Sweden 🇸🇪, but been living in Tokyo 🇯🇵 since 2015.</p>
+                <h5><span>Background</span></h5>
+                <p>Animation, design and movie history but my true love is code!
+                    I really love working on Frontend solutions and solving the puzzles of the backend.</p>
+                <h5><span>When I’m not coding</span></h5>
+                <p> I also love Judo, Video games and fine Cocktails!</p>
         </div>
-    )
+        <div className="col-sm-8 col-md-12 col-lg-6 about-skills">
+          <h2>Languages</h2>
+          <div className="mb-5 skill-icon-container">
+              <i className="devicon-javascript-plain"></i>
+              <i className="devicon-html5-plain"></i>
+              <i className="devicon-css3-plain"></i>
+              <i className="devicon-ruby-plain"></i>
+              <i className="devicon-postgresql-plain"></i>
+          </div>
+          <h2>Frameworks and Libraries</h2>
+          <div className="skill-icon-container">
+              <i className="devicon-react-original"></i>
+              <i className="devicon-rails-plain"></i>
+              <i className="devicon-bootstrap-plain"></i>
+              <i className="devicon-git-plain"></i>
+              <i className="devicon-github-original"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AboutCard

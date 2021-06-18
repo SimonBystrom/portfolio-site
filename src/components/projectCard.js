3867c462e6084bea1fef354b8a07beb1831d42b7
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-18 18:27:04 +0900

    Fixed sticky and built rough base for projectCard

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -4,9 +4,15 @@ const ProjectCard = (props) => {
 
   return(
     <div id="work" className="project-card-container">
-      <div className="row justify-content-around">
-        <div className="col-sm-12 col-md-6 ">
-
+      <div className="row justify-content-around p-5">
+        <div className="col-sm-12 col-md-6 project-card-info">
+          <h1>Project name</h1>
+          <p>Project description</p>
+          <div className="d-flex">
+            <p>tech</p>
+            <p>tech</p>
+            <p>tech</p>
+          </div>
         </div>
         <div className="col-sm-12 col-md-6">
           <div className="project-device">

fe62647d39c3155471b7505f9cbf6fa3764cb095
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-20 13:19:30 +0900

    added some styling on hover

--- src/components/socials.js
+++ src/components/socials.js
@@ -3,9 +3,17 @@ import React from 'react'
 const Socials = () => {
   return(
     <div className="socials-container">
-      <div id="github-social"></div>
-      <div id="linkedin-social"></div>
-      <div id="wantedly-social"></div>
+      <div label="github-link" id="github-social">
+        <i className="fab fa-github github-icon"></i>
+        <a id="github-link" href="https://github.com/SimonBystrom" target="_blank" rel="noreferrer"><span></span></a>
+      </div>
+      <div id="linkedin-social">
+        <i className="fab fa-linkedin-in linkedin-icon"></i>
+      </div>
+      <div id="insta-social">
+        <i class="fab fa-instagram insta-icon"></i>
+      </div>
+
     </div>
   )
 }

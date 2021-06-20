14120c0403e879e420009c3bac3d41e3182feaf3
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-20 11:56:35 +0900

    Added social links bars with animation

---
+++ src/components/socials.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Socials = () => {
+  return(
+    <div className="socials-container">
+      <div id="github-social"></div>
+      <div id="linkedin-social"></div>
+      <div id="wantedly-social"></div>
+    </div>
+  )
+}
+
+export default Socials

d6676e2d90e5a15ace133dc447631ea3faab58c9
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-06-23 18:01:29 +0900

    cleaned up project write-up

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     title: 'Split it',
-    description: 'Final project (2 weeks) built during my time at Le Wagon bootcamp. Mobile first app to help couples or groups of friends split costs for larger events like trips or monthly expenses. Allows the user to scan a photo of a receipt and then automatically fills in the total and the name of the store. Automatically splits the total between all the members of the group and says who should pay whom and how much. This project taught me a lot about Mobile first development. I also learned a lot about Google cloud services such as their Vision API and OCR.',
+    description: "Split It is an application developed by myself and my team in two weeks as a final project during my participation in Le Wagon bootcamp. It's a Mobile first app to help couples or groups of friends split costs for larger events like trips or monthly expenses. Allows you to scan a photo of a receipt and then automatically fills in the total and the name of the store. Automatically splits the total between all the members of the group and says who should pay whom and how much. This project taught me a lot about Mobile first development. I also learned a lot about Google cloud services such as their Vision API and OCR and setting up a PWA.",
     tech: ["devicon-rails-plain", "devicon-ruby-plain", "devicon-javascript-plain", "devicon-html5-plain", "devicon-sass-original", "devicon-googlecloud-plain", "devicon-heroku-original", "devicon-postgresql-plain", "devicon-git-plain", "devicon-github-original"],
     image: 'https://i.imgur.com/aG0f5a8.png',
     deployed: 'http://www.split-it.me/',
@@ -10,7 +10,7 @@ const projects = [
   },
   {
     title: 'Movie Ratings',
-    description: "Personal project I built for my wife and I. The website lets two users rate the same movie and saves your ratings in the browser. Once you've rated 5 movies it will give you recommendations based that both you and the other user liked! I learned a lot about async / await and how to properly call an API. I also gained some invaluable experience with deploying on a cloud-based service like Firebase.",
+    description: "Movie Ratings is a project I built for my wife and myself. The website lets two users rate the same movie and saves your ratings in the browser. Once you've rated 5 movies it will give you recommendations based on what both you and the other user liked. I learned a lot about async / await and how to properly call an API. I also gained some invaluable experience deploying on a cloud-based service like Firebase.",
     tech: ["devicon-react-original", "devicon-javascript-plain", "devicon-html5-plain", "devicon-css3-plain", "devicon-firebase-plain", "devicon-git-plain", "devicon-github-original"],
     image: 'https://i.imgur.com/BrfuKvK.png',
     deployed: 'https://movieratings-9f806.firebaseapp.com/',
@@ -19,7 +19,7 @@ const projects = [
   },
   {
     title: 'Chef_Space',
-    description: "AirBnB like marketplace site to help chefs without a restaurant open up pop-up restaurants for a short time by connecting them to location owners who wishes to rent out their space. (1 week project at Le Wagon). During this project I learnt a lot about how relational-databases work both N:1 and N:N. This was also the first site I had ever deployed to Heroku giving me experience with not just one but 2 different ways of deploying sites. One platform built on AWS with Heroku and one built by Google with Firebase.",
+    description: "Chef_Space is an AirBnB like marketplace site to help chefs without a restaurant open up pop-up restaurants for a short time by connecting them to location owners who wish to rent out their space. This was a one week project at the coding bootcamp I attended, Le Wagon. During this project I learnt a lot about how relational-databases work, both N:1 and N:N relations. This was also the first site I had ever deployed to Heroku giving me experience with not just one but 2 different ways of deploying sites. One platform built on AWS through Heroku and one built by Google through Firebase.",
     tech: ["devicon-rails-plain", "devicon-ruby-plain", "devicon-javascript-plain", "devicon-html5-plain", "devicon-sass-original", "devicon-googlecloud-plain", "devicon-heroku-original", "devicon-postgresql-plain", "devicon-git-plain", "devicon-github-original"],
     image: 'https://i.imgur.com/mV4QS4v.png',
     deployed: 'http://chefspace.herokuapp.com/',
@@ -28,7 +28,7 @@ const projects = [
   },
   {
     title: 'Youtube-clone',
-    description: "A UI / design challenge I gave myself to practice CSS and creating clean React Components. During this project I learnt how to use CSS Grid and Flexbox. I also gained a better understanding of the how much responsibility a React component should have in a larger context. The site is deployed on GitHub pages.",
+    description: "Youtube-Clone is a UI / design challenge I gave myself to practice CSS and to create clean React Components. During this project I learnt how to use CSS Grid and Flexbox. I also gained a better understanding of the how much responsibility a React component should have in a larger context. The site is deployed on GitHub pages.",
     tech: ["devicon-react-original", "devicon-javascript-plain", "devicon-html5-plain", "devicon-css3-plain", "devicon-git-plain", "devicon-github-original"],
     image: 'https://i.imgur.com/d6FGgZ2.png',
     deployed: 'https://simonbystrom.github.io/YouTube-clone-DESIGN-/',
